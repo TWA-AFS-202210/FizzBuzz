fe54b9ac88863d52b71ae893d482cb97a1d059e6
Qiu Wan <QWan@slb.com>
2022-10-26 17:56:27 +0800

    feat: implement code logic and test for fizz rule

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -7,16 +7,29 @@ namespace FizzBuzzTest
     public class FizzBuzzGameTest
     {
         [Fact]
-        public void Should_return_normal_number_When_countOff_Given_normal_number()
+        public void Should_return_normal_number_When_call_countOff_Given_normal_number()
         {
             //given
             FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
-            int normalNumber = 1;
+            int inputNumber = 1;
 
             //when
-            string countOffReturnResult = fizzBuzzGame.CountOff(normalNumber);
+            string countOffReturnResult = fizzBuzzGame.CountOff(inputNumber);
             //then
             Assert.Equal("1", countOffReturnResult);
+        }
+
+        [Fact]
+        public void Should_return_Fizz_When_call_countOff_Given_number_can_divided_by_3()
+        {
+            //given
+            FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+            int inputNumber = 3;
+
+            //when
+            string countOffReturnResult = fizzBuzzGame.CountOff(inputNumber);
+            //then
+            Assert.Equal("Fizz", countOffReturnResult);
         }
     }
 }
